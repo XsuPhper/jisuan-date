d67938691a05fd57d7f7a7c6dd33d0043c5bc49b
sjj <39753778@qq.com>
2017-08-01 17:50:06 +0800

    1

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+//计算两个时间
+ini_set('date.timezone','Asia/Shanghai');
+function format_date($time){
+    $t=time()-$time;
+    $f=array(
+        '31536000'=>'年',
+        '2592000'=>'个月',
+        '604800'=>'星期',
+        '86400'=>'天',
+        '3600'=>'小时',
+        '60'=>'分钟',
+        '1'=>'秒'
+    );
+    foreach ($f as $k=>$v)    {
+        if (0 !=$c=floor($t/(int)$k)) {
+            return $c.$v.'前';
+        }
+    }
+}
+
+$a = format_date(strtotime('2017-07-10 17:00:00'));
+print_r($a);
+
